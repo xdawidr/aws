e01cfcde74c480bb4e977a737c5c59bf44e6b388
xdawidr <dawid.rychlinski@gmail.com>
2025-10-28 12:28:50 +0100

    edit bucket in backend.tf

--- tfmaestro/envs/prod/backend.tf
+++ tfmaestro/envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "<BUCKET_ID>"
+        bucket = "terraform-state-prod-27102512320251027162758418100000001"
         key = "terraform.tfstate"
         region = "us-east-1"
     }
